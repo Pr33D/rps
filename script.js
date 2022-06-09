ab71e7ffd8105cbe37a8557b3112c09e0b96762a
Pr33D <keymaps@magenta.de>
2022-06-09 19:48:19 +0200

    function play ready

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ let rps = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
   let chance = Math.floor(Math.random() * 3);
+  console.log(rps[chance]);
   return rps[chance];
 }
 
 function oneRound(playerSelection, computerSelection) {
+  let playerChoice = "";
+  let result = "";
   rps.forEach((element) => {
     if (element.toLowerCase() === playerSelection.toLowerCase()) {
-      console.log(element);
+      playerChoice = element;
     }
   });
+  console.log(playerChoice);
+  if (playerChoice == computerSelection) result = "Draw! Try again.";
+  else if (
+    (playerChoice == "Rock" && computerSelection == "Scissors") ||
+    (playerChoice == "Paper" && computerSelection == "Rock") ||
+    (playerChoice == "Scissors" && computerSelection == "Paper")
+  ) {
+    result = `You win! ${playerChoice} beats ${computerSelection}`;
+  } else result = `You lose! ${computerSelection} beats ${playerChoice}`;
+  console.log(result);
 }
 
 let play = oneRound("paper", computerPlay());
