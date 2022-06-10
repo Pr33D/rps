ecd405cb4267e7c648c0bd4f3855ff0d9fb1db67
Pr33D <keymaps@magenta.de>
2022-06-10 21:25:04 +0200

    Functions working, Popup working

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let rps = ["Rock", "Paper", "Scissors"];
 let wins = 0;
-document.querySelector("#wins").innerHTML = `Games won: ${wins}`;
 let draws = 0;
-document.querySelector("#draws").innerHTML = `Draw Games: ${draws}`;
 let looses = 0;
-document.querySelector("#looses").innerHTML = `Games lost: ${looses}`;
+let resultPop = document.querySelector(".gameResult");
+let endMsg = document.querySelector(".gameResultMsg");
 
 function computerPlay() {
-  let chance = Math.floor(Math.random() * 3);
+  let chance = Math.floor(Math.random() * rps.length);
   return rps[chance];
 }
 
@@ -16,21 +15,50 @@ function playRound(playerSelection, computerSelection) {
   result.innerHTML = `You show ${playerSelection}, the Computer shows ${computerSelection}<br>`;
   if (playerSelection == computerSelection) {
     result.innerHTML += `Draw!`;
-    return;
+    draws++;
+    showCounters();
   } else if (
     (playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Paper" && computerSelection == "Rock") ||
     (playerSelection == "Scissors" && computerSelection == "Paper")
   ) {
     result.innerHTML += `You win! ${playerSelection} beats ${computerSelection}`;
-    return wins++;
+    wins++;
+    showCounters();
   } else {
     result.innerHTML += `You lose! ${computerSelection} beats ${playerSelection}`;
-    return looses++;
+    looses++;
+    showCounters();
   }
+}
+
+function resetGame() {
+  wins = 0;
+  draws = 0;
+  looses = 0;
+  result.innerHTML = "";
+  showCounters();
+  resultPop.classList.add("nodis");
+}
+
+function showCounters() {
+  document.querySelector("#wins").innerHTML = wins;
+  document.querySelector("#draws").innerHTML = draws;
+  document.querySelector("#looses").innerHTML = looses;
+  if (wins === 5) return winLoose("won");
+  else if (looses === 5) return winLoose("lost");
 }
 
 let buttons = document.querySelectorAll(".btn");
 buttons.forEach((btn) =>
   btn.addEventListener("click", () => playRound(btn.innerHTML, computerPlay()))
 );
+
+let resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", () => resetGame());
+
+function winLoose(gameResult) {
+  resultPop.classList.remove("nodis");
+  if (gameResult === "won") endMsg.textContent = "You won this game!";
+  else endMsg.textContent = "You lost this game!";
+}
